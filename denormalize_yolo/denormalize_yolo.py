97cc972d676c4c997d5a2c42424eeefc12a00279
mohsin66 <mmmbadar@gmail.com>
2021-08-03 13:22:17 +0500

    denormalize and commands updated

--- denormalize_yolo/denormalize_yolo.py
+++ denormalize_yolo/denormalize_yolo.py
@@ -1,12 +1,15 @@
 import os
 
 def get_files():
-    files = os.listdir("train/")
+    if not os.path.exists('result_new'):
+        os.mkdir('result_new')
+    files = os.listdir("1cls_personBag_3243_concise/labels/train/")
+    iter=1
     for file in files:
-        with open("train/"+file, "r") as f:
+        with open("1cls_personBag_3243_concise/labels/train/"+file, "r") as f:
             lines = f.readlines() #reads one line at a time
             # print(lines[0].split(' ')[1])
-        with open("test_detectron/"+file, "w") as f:
+        with open("result_new/"+file, "w") as f:
             for line in lines:
                 xc = float(line.split(' ')[1])
                 yc = float(line.split(' ')[2])
@@ -23,6 +26,7 @@ def get_files():
                 width = str(int(int_width))
                 height = str(int(int_height))
                 
-                f.write("0 "+min_x+" "+min_y+" "+width+" "+height+"\n")
+                f.write(str(0)+" "+min_x+" "+min_y+" "+width+" "+height+"\n")
+            iter=iter+1
 
 get_files()
